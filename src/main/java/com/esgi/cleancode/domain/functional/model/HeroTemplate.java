930f8c2220e0002e4a10e486a0ae571127441a03
Dénice <delton.denice@gmail.com>
2023-02-25 14:21:56 +0100

    Initial

--- src/main/java/com/esgi/cleancode/domain/functional/model/HeroTemplate.java
+++ src/main/java/com/esgi/cleancode/domain/functional/model/HeroTemplate.java
@@ -1,11 +1,14 @@
 package com.esgi.cleancode.domain.functional.model;
 
+import com.esgi.cleancode.server.driver.annotation.DbColumnName;
+import com.esgi.cleancode.server.driver.annotation.DbColumnType;
 import com.esgi.cleancode.server.driver.annotation.DbTableName;
 
 @DbTableName(value = "hero_template")
 public class HeroTemplate {
 
+    @DbColumnName(value = "id")
+    @DbColumnType(type = Integer.class)
     private Integer id;
 
-    private
 }
