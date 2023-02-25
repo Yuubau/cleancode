550f78ded7877ab31b8102669a0322519f1e9f96
Dénice <delton.denice@gmail.com>
2023-02-25 15:22:19 +0100

    create hero domain model

---
+++ src/main/java/com/esgi/cleancode/domain/functional/model/Hero.java
@@ -0,0 +1,10 @@
+package com.esgi.cleancode.domain.functional.model;
+
+public class Hero {
+
+    private Integer id;
+    private Integer experiencePoints;
+    private Integer playerId;
+    private Integer heroId;
+    private Integer level;
+}
