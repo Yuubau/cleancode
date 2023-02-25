905b0947f6a8ce131e173e70cd28c26735013c29
Dénice <delton.denice@gmail.com>
2023-02-25 15:22:59 +0100

    create hero controller

---
+++ src/main/java/com/esgi/cleancode/client/resource/HeroResource.java
@@ -0,0 +1,12 @@
+package com.esgi.cleancode.client.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/hero")
+public class HeroResource {
+
+}
